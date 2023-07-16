cd1fadde2e6c45d148a243b3dc97bcc5a2f622dd
Григорчук Кирилл <kgrigorchuk@it-one.ru>
2023-07-16 21:07:37 +0300

    Added two test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -4,13 +4,19 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
 public class FirstTest {
 
     private AppiumDriver driver;
+    public static final long DEFAULT_WAIT_TIME = 15;
+    public static final String ERROR_MESSAGE = "Element not found";
 
     @Before
     public void setUp() throws Exception
@@ -36,6 +42,70 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        System.out.println("first test run");
+        waitAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                ERROR_MESSAGE,
+                DEFAULT_WAIT_TIME
+        );
+
+        waitAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                ERROR_MESSAGE,
+                DEFAULT_WAIT_TIME
+        );
+
+        waitUntilElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
+                                "//*[contains(@text,'Object-oriented programming language')]"
+                ),
+                "Current article not found",
+                DEFAULT_WAIT_TIME);
     }
+
+    @Test
+    public void testCancelSearch(){
+        waitAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search input",
+                DEFAULT_WAIT_TIME
+        );
+
+        waitAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X to cancel search",
+                DEFAULT_WAIT_TIME
+        );
+
+        waitElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X is still present on the page",
+                5
+        );
+    }
+
+    private WebElement waitUntilElementPresent(By locator, String errorMsg, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMsg + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    private boolean waitElementNotPresent(By locator, String errorMsg, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMsg + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    private WebElement waitAndClick(By locator, String errorMsg, long timeoutInSeconds){
+        WebElement element = waitUntilElementPresent(locator, errorMsg, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitAndSendKeys(By by, String value, String errorMsg, long timeoutInSeconds){
+        WebElement element = waitUntilElementPresent(by, errorMsg, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+    
 }
